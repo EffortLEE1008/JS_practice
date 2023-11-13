bf00b561e28ad808c7cfda6ea9bc39c7dbe1104c
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2023-11-13 15:29:30 +0900

    classList_toggle
    #은 id, .은 classname
    
    화이팅!

--- app2.js
+++ app2.js
@@ -1,14 +1,25 @@
 
 
-const h1 = document.querySelector("h1.title2");
+const h1 = document.querySelector("#title2 h1");
 
 h1.innerText = "change inner Text";
 
 
 function handleTitleClick(){
-    h1.className= "active";
+    const s_active = "active";
+
+    // if(h1.classList.contains(s_active)){
+    //     h1.classList.remove(s_active);
+    // }else{
+    //     h1.classList.add(s_active);
+    // }                                        //toggle의 동작 원리와 같은 기능
+    h1.classList.toggle(s_active);
+
+    console.log("현재 클래스 내임은 "+h1.className);
+    console.log(h1.classList.length);
 }
 
 h1.addEventListener("click", handleTitleClick);
 
-console.log(h1.classNAme);
+console.dir(h1);
+console.log(h1.classList[0]);
